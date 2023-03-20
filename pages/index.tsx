36d562a698bd58232da10a8940801473d74a947c
Bahhe <marchelldteach@gmail.com>
2023-03-20 20:32:39 +0100

    project finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,9 +16,9 @@ export default function Home() {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     setDisabled(true);
-    setTimeout(() => {
+    const warningMsg = setTimeout(() => {
       setWarning(true);
-    }, 5000);
+    }, 10000);
     e.preventDefault();
     const question = inputValue;
     setChat((prev) => [...prev, question]);
@@ -36,6 +36,7 @@ export default function Home() {
       .finally(() => {
         setDisabled(false);
         setFocus(true);
+        clearTimeout(warningMsg);
         setWarning(false);
       });
   };
@@ -51,12 +52,14 @@ export default function Home() {
       <main className={inter.className}>
         <div className="">
           <div className="flex flex-col items-center justify-center">
-            <h1 className="text-5xl font-bold text-center py-10">
+            <h1 className="text-5xl font-bold text-center py-10 text-white">
               Welcome to BrainstormAI
             </h1>
-            <p className="text-lg w-96 text-teal-300">
-              you have A brainstorm? discuss it with brainstromAI and see what
-              it tells you about it
+            <p className="text-lg w-[500px] text-teal-300">
+              Welcome to our chatbot app! Our GPT-3.5-powered chatbot is here to
+              assist you and provide personalized responses to your queries. So
+              what are you waiting for? Say hello and let&apos;s start a
+              conversation!
             </p>
             <div>
               <div className="w-96 h-[500px] border border-gray-600 bg-gray-800 mt-10 rounded-lg overflow-y-scroll">
@@ -67,7 +70,7 @@ export default function Home() {
                       .map((text, index) => (
                         <li
                           key={index}
-                          className="odd:bg-teal-500 even:bg-teal-700 p-2 odd:rounded-tl-lg odd:rounded-tr-lg odd:rounded-br-lg even:rounded-tl-lg even:rounded-tr-lg even:rounded-bl-lg w-fit even:place-self-end"
+                          className="text-sm w-fit odd:border even:border even:border-pink-500 even:text-gray-300 p-2 odd:rounded-tl-lg odd:rounded-tr-lg odd:rounded-br-lg text-white even:rounded-tl-lg even:rounded-tr-lg even:rounded-bl-lg even:place-self-end"
                         >
                           {text}
                         </li>
@@ -76,7 +79,7 @@ export default function Home() {
               </div>
               <form
                 onSubmit={handleSubmit}
-                className="w-96 h-12 px-2 border border-gray-600 bg-gray-800 mt-5 rounded-full flex items-center justify-between"
+                className="w-96 h-12 px-2 mb-72 border border-gray-700 bg-gray-800 mt-5 rounded-full flex items-center justify-between"
               >
                 {disabled && (
                   <SyncLoader className="text-sm" color="teal" size={7} />
@@ -85,7 +88,7 @@ export default function Home() {
                   type="text"
                   value={inputValue}
                   onChange={(e) => setInputValue(e.target.value)}
-                  className="rounded-full p-2 m-2 w-full bg-transparent outline-none"
+                  className="rounded-full p-2 m-2 w-full bg-transparent outline-none text-white"
                   placeholder={
                     !disabled
                       ? "what's your brainstorm?..."
@@ -95,11 +98,11 @@ export default function Home() {
                   autoFocus={focus}
                 />
                 <button type="submit" disabled={disabled}>
-                  <AiOutlineSend className="m-2 text-3xl" />
+                  <AiOutlineSend className="m-2 text-3xl text-white outline-none" />
                 </button>
               </form>
               {warning && (
-                <p className="text-red-500 text-xs my-4 w-96">
+                <p className="text-red-500 text-xs my-4 w-96 fixed bottom-2 p-3 bg-black rounded">
                   NOTE: if you waited too much it is due to api problem, please
                   refresh the page
                 </p>
